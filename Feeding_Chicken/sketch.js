df26b5d5232bcd597102eb3d320c6f134ba509f2
Fiorella Barrientos Villalta <u201711590@upc.edu.pe>
2019-12-01 14:55:40 -0500

    done Feeding Chickens

--- Feeding_Chicken/sketch.js
+++ Feeding_Chicken/sketch.js
@@ -1,6 +1,6 @@
 var letters = [], M = [], ans = [], states = [], colores = [];
-var n = Math.floor(Math.random()* 6) + 10;
-var m = Math.floor(Math.random()* 6) + 10;
+var n = Math.floor(Math.random()* 10) + 3;
+var m = Math.floor(Math.random()* 10) + 3;
 var k = Math.floor(Math.random()* 8) + 3;
 var suma = 0, it = 0;
 var color1 = [0,255,255,255,181,51,51,134,236,108,19];
@@ -8,6 +8,7 @@ var color2 = [0,51,193,252,255,255,125,51,51,118,61];
 var color3 = [0,51,51,51,51,240,255,255,255,118,134];
 
 function init(){
+  if(k > n*m) k = n*m;
   var i, inter = int(256/k), cont_k = 0;
   for(i = 0; i <= k; ++i)
       letters.push(i + 1);
@@ -92,18 +93,18 @@ function setup() {
   createCanvas(400, 400);
   init();
   solve();
-  frameRate(n*m);
+  frameRate(n*m/k);
 }
 function draw() {
-  background(220);
-  var rows = Math.ceil(height/n), b;
-  var columns = Math.ceil(width/m);
+  background(0,0,0);
+  var rows = Math.floor(height/n), b;
+  var columns = Math.floor(width/m);
   for(var i = 0; i < n; ++i){
     for(var j = 0; j < m; ++j){
       var idx = states[it][i][j];
       b = color(color1[idx], color2[idx], color3[idx]);
       fill(b)
-      rect(j*columns, i*rows, columns, rows);
+      rect(j*columns, i*rows, (j + 1)*columns - j*columns, (i + 1)*rows - i*rows);
       if(M[i][j] == 'R'){
         var mid_x = ((j + 1)*columns + j*columns)/2;
         var mid_y = ((i + 1)*rows + i*rows)/2;
